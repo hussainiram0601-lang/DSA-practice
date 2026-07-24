a39224ba7c95355bb86e68532e244d7c502e0042
Iram Hussain <hussainiram0601@gmail.com>
2026-07-24 15:10:02 +0530

    Time: 0 ms (100%), Space: 20.4 MB (55.67%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dfs(self, i , adjMatrix , visited):
+        visited[i] = True
+        for x in range(len(adjMatrix[i])):
+            if adjMatrix[i][x]==1 and not visited[x]:
+               self.dfs(x , adjMatrix , visited)
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = [False]*n
+        ans = 0
+        for i in range(n):
+            if not visited[i]:
+                self.dfs(i , isConnected , visited)
+                ans+=1
+        return ans
+        

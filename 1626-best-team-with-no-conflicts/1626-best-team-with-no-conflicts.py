a658a7280e75b20c71038775e8f1630517effc0f
Iram Hussain <hussainiram0601@gmail.com>
2026-09-23 18:12:03 +0530

    Time: 758 ms (55.99%), Space: 19.6 MB (39.61%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,13 +1,12 @@
 class Solution:
     def bestTeamScore(self, scores: list[int], ages: list[int]) -> int:
-        pair = [[scores[i],ages[i]]  for i in range(len(scores))]
+        pair = [[scores[i], ages[i]] for i in range(len(scores))]
         pair.sort()
         dp = [pair[i][0] for i in range(len(pair))]
         for i in range(len(pair)):
-            Mscore, Mage = pair[i]
+            Mscore ,Mage = pair[i]
             for j in range(i):
-                score, age = pair[j]
-                if Mage>= age:
-                    dp[i]= max( dp[i], dp[j]+Mscore )
-        return max(dp)
-        
+                score , age = pair[j]
+                if Mage >=age:
+                    dp[i] = max(dp[i], dp[j]+ Mscore)
+        return max(dp)        
